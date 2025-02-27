4087a5d6692bb8b94903d59175bbfb64c9f95889
Vinay Reddy Kalluri <vinayreddykalluri@gmail.com>
2025-02-27 15:36:39 -0500

    coding-challenges-java

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        int[] a = new int[5];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = i;
+        }
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+
+        int b = 0;
+        System.out.println(b);
+    }
+}
