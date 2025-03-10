b7ecc7e4423fceea2baaaa71ed2329adeeb7f020
Vinay Reddy Kalluri <vinayreddykalluri@gmail.com>
2025-03-10 19:29:31 -0400

    Reverse A Number - correct

--- src/ReverseANumber.java
+++ src/ReverseANumber.java
@@ -4,12 +4,10 @@ public class ReverseANumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
-
         if (number == 0) {
             System.out.println("0");
             return;
         }
-
         // Directly print trailing zeros first
         int trailingZeros = 0;
         while (number % 10 == 0) {
@@ -17,13 +15,11 @@ public class ReverseANumber {
             number /= 10;
         }
         while (trailingZeros -- > 0) System.out.print("0");
-
         // Reverse number and print digits directly (avoiding extra calculations)
         while (number > 0) {
             System.out.print(number % 10);
             number /= 10;
         }
-
         System.out.println(); // Move to the next line
     }
 }
