55e9ec27f917fbedb309c4eef2cc5561e4318ad3
Vinay Reddy Kalluri <vinayreddykalluri@gmail.com>
2025-03-09 21:58:57 -0400

    Reverse A Number

--- src/ReverseANumber.java
+++ src/ReverseANumber.java
@@ -5,12 +5,21 @@ public class ReverseANumber {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
         int reversedNumber = 0;
+        boolean isTrailingZero = (number % 10 == 0); // Check if number has trailing zeros
+
+        // Reverse number manually
         while (number != 0) {
             int digit = number % 10;
             reversedNumber = reversedNumber * 10 + digit;
             number /= 10;
         }
-        System.out.println(reversedNumber);
 
+        // Print manually to retain trailing zeros
+        if (isTrailingZero) {
+            System.out.print(reversedNumber);
+            System.out.print("0"); // Manually add a trailing zero
+        } else {
+            System.out.println(reversedNumber);
+        }
     }
 }
