eefff5204452ad3e75ae715b1d2f6532bf0562af
Vinay Reddy Kalluri <vinayreddykalluri@gmail.com>
2025-03-09 21:59:25 -0400

    Reverse A Number

--- src/ReverseANumber.java
+++ src/ReverseANumber.java
@@ -4,22 +4,29 @@ public class ReverseANumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
-        int reversedNumber = 0;
-        boolean isTrailingZero = (number % 10 == 0); // Check if number has trailing zeros
 
-        // Reverse number manually
+        // Count trailing zeros
+        int trailingZeros = 0;
+        while (number % 10 == 0 && number != 0) {
+            trailingZeros++;
+            number /= 10; // Remove trailing zeros for now
+        }
+
+        // Reverse the number manually
+        int reversedNumber = 0;
         while (number != 0) {
-            int digit = number % 10;
-            reversedNumber = reversedNumber * 10 + digit;
+            reversedNumber = reversedNumber * 10 + number % 10;
             number /= 10;
         }
 
-        // Print manually to retain trailing zeros
-        if (isTrailingZero) {
-            System.out.print(reversedNumber);
-            System.out.print("0"); // Manually add a trailing zero
-        } else {
-            System.out.println(reversedNumber);
+        // Print reversed number
+        System.out.print(reversedNumber);
+
+        // Manually append trailing zeros
+        for (int i = 0; i < trailingZeros; i++) {
+            System.out.print("0");
         }
+
+        System.out.println();
     }
 }
