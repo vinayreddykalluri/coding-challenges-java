c80b22a7ea2ac77aa2b629ca868efde69c745d24
Vinay Reddy Kalluri <vinayreddykalluri@gmail.com>
2025-03-11 20:07:12 -0400

    Reverse A Number - correct

---
+++ src/ReverseNumberSimple.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ReverseNumberSimple {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int reversed = 0;
+
+        while (number != 0) {
+            int digit = number % 10; // Extract last digit
+            reversed = reversed * 10 + digit; // Build reversed number
+            number /= 10; // Remove last digit
+        }
+
+        System.out.println(reversed);
+    }
+}
