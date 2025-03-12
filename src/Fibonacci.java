0e53bacce429f05ba5457afa8a6e573d74e585de
Vinay Reddy Kalluri <vinayreddykalluri@gmail.com>
2025-03-12 18:02:17 -0400

    Fibonacci - Fast Algorithm

---
+++ src/Fibonacci.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static int fibonacci(int n) {
+        double sqrt5 = Math.sqrt(5);
+        System.out.println(sqrt5);
+        return (int) ((Math.pow((1 + sqrt5) / 2, n) - Math.pow((1 - sqrt5) / 2, n)) / sqrt5);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(fibonacci(n));
+    }
+}
