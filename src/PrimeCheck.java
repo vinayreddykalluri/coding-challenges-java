f5b81e3dfe19b1fcb3c98da8ac2d030c8ebbdb53
Vinay Reddy Kalluri <vinayreddykalluri@gmail.com>
2025-03-11 20:11:26 -0400

    Prime Check

---
+++ src/PrimeCheck.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class PrimeCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        boolean isPrime = isPrime(number);
+        System.out.println(isPrime ? "Prime" : "Not Prime");
+    }
+
+    private static boolean isPrime(int number) {
+        return number > 1 && java.math.BigInteger.valueOf(number).isProbablePrime(1);
+    }
+}
