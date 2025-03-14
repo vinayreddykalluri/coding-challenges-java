78b300d62372dc7f99da1426be14105274ea5256
Vinay Reddy Kalluri <vinayreddykalluri@gmail.com>
2025-03-13 21:46:45 -0400

    ReverseIntegerWithOutOverFlow

---
+++ src/ReverseIntegerWithOutOverFlow.java
@@ -0,0 +1,20 @@
+public class ReverseIntegerWithOutOverFlow {
+        public static int reverse(int n) {
+            int reversed = 0;
+            while (n != 0) {
+                int digit = n % 10;
+                if (reversed > Integer.MAX_VALUE / 10 || reversed < Integer.MIN_VALUE / 10) {
+                    return 0; // Overflow detected
+                }
+                reversed = reversed * 10 + digit;
+                n /= 10;
+            }
+            return reversed;
+        }
+
+        public static void main(String[] args) {
+            System.out.println(reverse(1534236469)); // Output: 0
+            System.out.println(reverse(-123)); // Output: -321
+        }
+    }
+
