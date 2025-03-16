d1d4c224e1d2fb829c433527d646023880e85e30
Vinay Reddy Kalluri <vinayreddykalluri@gmail.com>
2025-03-16 11:52:03 -0400

    IntegerToRoman

---
+++ src/Numbers/IntegerToRoman.java
@@ -0,0 +1,21 @@
+package Numbers;
+
+public class IntegerToRoman {
+    public static String intToRoman(int num) {
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < values.length; i++) {
+            while (num >= values[i]) {
+                num -= values[i];
+                sb.append(symbols[i]);
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(intToRoman(1994)); // Output: MCMXCIV
+    }
+}
